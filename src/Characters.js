31f4babdea2d50da68560737a36e6e4545cf80af
BloxyRoadDev <112256827+BloxyRoadDev@users.noreply.github.com>
2022-09-26 12:31:23 +0200

    Update Characters.js

--- src/Characters.js
+++ src/Characters.js
@@ -8,23 +8,7 @@ export default {
     id: "bacon"
   },
   brent: {
-    name: "Brent Vatne",
-    id: "brent"
+    name: "Chicken",
+    id: "chicken"
   },
-  avocoder: {
-    name: "Avocoder",
-    id: "avocoder"
-  },
-  wheeler: {
-    name: "Wheeler",
-    id: "wheeler"
-  },
-  palmer: {
-    name: "Palmer",
-    id: "palmer"
-  },
-  juwan: {
-    name: "Juwan",
-    id: "juwan"
-  },
-};
+  
